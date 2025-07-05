b6915c282ef2ae6849fa85c65347694e5261c039
rel4t <volckovle5h@yandex.ru>
2025-07-05 12:28:45 +0300

    Обновлена конфигурация бота

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,16 +1,19 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties 
 
-current_dir = os.path.dirname(__file__)
-parent_dir = os.path.dirname(current_dir)
-env_path = os.path.join(parent_dir, ".env")
 
+env_path = Path(__file__).parent.parent.parent / '.env'
 load_dotenv(env_path)
 
 token = os.getenv("BOT_FATHER")
+if not token:
+    raise ValueError("Токен бота не найден!")
+
 bot = Bot(token=token, default=DefaultBotProperties(parse_mode="HTML"))
 
 
+    
