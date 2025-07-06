177b0792418b849a95014a3f3c2c3f00f6f985c5
rel4t <volckovle5h@yandex.ru>
2025-07-06 17:35:08 +0300

    Обновлены модели таблиц базы данных

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, BigInteger, String, Boolean, ForeignKey, Numeric, Text, DateTime
+from sqlalchemy import (
+    Column, Integer, String, Boolean, Numeric, ForeignKey,
+    UniqueConstraint, TIMESTAMP, BigInteger, DateTime, func
+)
 from sqlalchemy.orm import relationship
 from database.database import Base
 
@@ -15,5 +18,49 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
     updated_at = Column(DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        return f"<User(id={self.id}, username='{self.username}')>"
+
+class Catalog(Base):
+    __tablename__ = "catalogs"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+
+    categories = relationship("Category", back_populates="catalog")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    __table_args__ = (UniqueConstraint("catalog_id", "name"),)
+
+    id = Column(Integer, primary_key=True)
+    catalog_id = Column(Integer, ForeignKey("catalogs.id", ondelete="CASCADE"), nullable=False)
+    name = Column(String, nullable=False)
+
+    catalog = relationship("Catalog", back_populates="categories")
+    products = relationship("Product", back_populates="category")
+
+
+class Unit(Base):
+    __tablename__ = "units"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+
+    products = relationship("Product", back_populates="unit")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey("categories.id", ondelete="CASCADE"), nullable=False)
+    unit_id = Column(Integer, ForeignKey("units.id"), nullable=False)
+    available = Column(Boolean, nullable=False, default=True, server_default="true")
+    quantity = Column(Numeric(10, 2), nullable=False, default=0)
+    size = Column(Numeric(10, 2), nullable=False, default=1)  # Новое поле с дефолтом
+    price = Column(Numeric(10, 2), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    category = relationship("Category", back_populates="products")
+    unit = relationship("Unit", back_populates="products")
