a238d7945f4da73ce92b99a3cd4d75e61aa55da8
rel4t <volckovle5h@yandex.ru>
2025-07-07 13:08:22 +0300

    Рефакторинг обработчиков: переход на inline-клавиатуру, исправления

--- src/keyboards/admin_update_catalog.py
+++ src/keyboards/admin_update_catalog.py
@@ -6,7 +6,7 @@ work_with_catalog_buttons = [
         InlineKeyboardButton(text="Отобразить список доступных категорий", callback_data="catalog_list")
     ],
     [
-        InlineKeyboardButton(text="Добавить категорию", callback_data="create_category")
+        InlineKeyboardButton(text="Добавить товар", callback_data="create_category")
     ],
     [
         InlineKeyboardButton(text="Обновить категорию", callback_data="update_category")
@@ -26,5 +26,13 @@ back_to_catalog_button = [
     ]
 ]
 
+yes_or_cancel_buttons = [
+    [
+        InlineKeyboardButton(text="Да", callback_data="yes_create_product"),
+        InlineKeyboardButton(text="Отмена", callback_data="cancel_create_product")
+    ]
+]
+
 work_with_catalog_keyboard = InlineKeyboardMarkup(inline_keyboard=work_with_catalog_buttons)
 back_to_catalog_keyboard = InlineKeyboardMarkup(inline_keyboard=back_to_catalog_button)
+yes_or_cancel_keyboard = InlineKeyboardMarkup(inline_keyboard=yes_or_cancel_buttons)
