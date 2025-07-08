810e66ed67ebd53f0118dc0bc99edb7419ebce0b
rel4t <volckovle5h@yandex.ru>
2025-07-08 12:47:00 +0300

    Добавлена универсальная функция для обновления информации в БД

--- src/services/product/common.py
+++ src/services/product/common.py
@@ -17,3 +17,15 @@ async def create_object(db: AsyncSession, model: Type[T], **kwargs) -> T:
     await db.commit()
     await db.refresh(obj)
     return obj
+
+
+async def update_object(db: AsyncSession, model: Type[T], obj_id: int, **fileds) -> T | None:
+    obj = await db.get(model, obj_id)
+    if obj:
+        for key, value in fileds.items():
+            setattr(obj, key, value)
+        
+        await db.commit()
+        await db.refresh(obj)
+
+    return obj
