1be9db44aeba01b5ff7a84a6e08d7af7e9cb4183
rel4t <volckovle5h@yandex.ru>
2025-07-09 10:38:05 +0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ from bot.dispatcher import dp
 
 from handlers.admin import admin_handlers
 from handlers.commom import common_handlers
+from handlers.update_product import register_update_product_handlers
 
 
 admin_handlers(dp)
 common_handlers(dp)
+register_update_product_handlers(dp)
 
 async def main():
     await dp.start_polling(bot)
