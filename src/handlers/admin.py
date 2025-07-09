e942fe013cd729d0817d004dee1a6b26d88f1162
rel4t <volckovle5h@yandex.ru>
2025-07-09 11:34:43 +0300

    некоторые грамматические исправления

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -199,12 +199,13 @@ async def enter_price(mes: Message, state: FSMContext):
         )
 
     text = (
-        f"Товар успешно добавлен!\n\n"
         f"Каталог: {names['catalog_name']}\n"
         f"Категория: {names['category_name']}\n"
         f"Размер одной: {pretty_num(data['size'])} {names['unit_name']}\n"
         f"Количество: {pretty_num(data['quantity'])}\n"
         f"Цена: {pretty_num(data['price'])}\n"
+        "Добавить этот товар?"
+
     )
 
     kb = yes_or_cancel_keyboard
@@ -237,12 +238,12 @@ async def confirm_product(query: CallbackQuery, state: FSMContext):
             )
 
         text = (
+            f"Товар успешно добавлен!\n\n"
             f"Каталог: {names['catalog_name']}\n"
             f"Категория: {names['category_name']}\n"
             f"Размер одной: {pretty_num(data['size'])} {names['unit_name']}\n"
             f"Количество: {pretty_num(data['quantity'])}\n"
             f"Цена: {pretty_num(data['price'])}\n"
-            "Добавить этот товар?"
         )
 
         await query.message.answer(text=text)
