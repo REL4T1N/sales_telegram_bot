575974faf18652164c84726b52b3a0cfe85991d8
rel4t <volckovle5h@yandex.ru>
2025-07-11 17:04:12 +0300

    Добавлен полный цикл создания категории

---
+++ src/handlers/admin/product/create/category.py
@@ -0,0 +1,104 @@
+from aiogram import Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+
+from database.database import get_db
+from database.models import Catalog, Category, Unit, Product
+
+from states.admin.product.create_product import CreateProduct
+
+from services.product.base import get_all, create_object
+
+from services.product.create import get_categories_by_catalog
+
+from utils.formatting_float_nums import pretty_num
+
+from schemas.product import ProductCreate
+
+from handlers.admin.base import IsAdmin
+
+from keyboards.admin.base import create_keyboard, create_back_button, confirming_keyboard
+
+from handlers.admin.product.create.catalog import show_categories_list, back_to_show_catalogs_list
+
+
+async def choose_category(query: CallbackQuery, state: FSMContext):
+    if query.data == "back_to_show_catalogs_list":
+        await back_to_show_catalogs_list(query, state)
+
+    elif query.data == "add_new_category":
+        data = await state.get_data()
+        async for db in get_db():
+            catalog = await db.get(Catalog, data["catalog_id"])
+
+        kb = await create_back_button("back_to_categories_list")
+
+        await state.set_state(CreateProduct.entering_category_name)
+        await query.message.edit_text(
+            text=f"Введите название новой категории для каталога <b>{catalog.name}</b>:",
+            reply_markup=kb
+        )
+        await query.answer()
+    
+    else:
+        category_id = int(query.data)
+        # нажата кнопка с id категории
+
+
+async def back_to_categories_list(query: CallbackQuery, state: FSMContext):
+    if query.data == "back_to_categories_list":
+
+        data = await state.get_data()
+        await state.clear()
+        await state.update_data(catalog_id=data["catalog_id"])
+        await show_categories_list(query, state)
+
+    else:
+        query.answer(text="Упс... Что-то пошло не так.\nПерезапустите приложение или свяжитесь с @REL4T1NCH1k")
+
+
+async def enter_new_category_name(mes: Message, state: FSMContext):
+    category_name = mes.text
+    data = await state.get_data()
+    await state.update_data(category_name=category_name)
+    
+    async for db in get_db():
+        catalog = await db.get(Catalog, data["catalog_id"])
+
+    kb = await confirming_keyboard("✅ Да", "confirm_add_category", "❌ Нет", "cancel_add_category")
+
+    await state.set_state(CreateProduct.confirming_category_name)
+    await mes.answer(
+        text=f"Добавить категорию <b>{category_name}</b> для каталога <b>{catalog.name}</b>?",
+        reply_markup=kb
+    )
+
+
+async def confirm_category_name(query: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+
+    if query.data == "confirm_add_category":
+        async for db in get_db():
+            category = await create_object(db, Category, catalog_id=data["catalog_id"], name=data["category_name"])
+
+        await query.answer(text="✅ Категория успешно добавлена")
+        await state.clear()
+        await state.update_data(catalog_id=data["catalog_id"])
+        await show_categories_list(query, state)
+
+    elif query.data == "cancel_add_category":
+        await query.answer(text="❌ Добавление категории отменено")
+        await state.clear()
+        await state.update_data(catalog_id=data["catalog_id"])
+        await show_categories_list(query, state)
+
+    else:
+        query.answer(text="Упс... Что-то пошло не так.\nПерезапустите приложение или свяжитесь с @REL4T1NCH1k")
+
+
+def register_product_create_category(dp: Dispatcher):
+    dp.callback_query.register(choose_category, CreateProduct.choosing_category, IsAdmin())
+    dp.callback_query.register(back_to_categories_list, CreateProduct.entering_category_name, IsAdmin())
+    dp.message.register(enter_new_category_name, CreateProduct.entering_category_name, IsAdmin())
+    dp.callback_query.register(confirm_category_name, CreateProduct.confirming_category_name, IsAdmin())
