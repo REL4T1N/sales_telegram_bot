5bd6c25f95e2eb32c01e064a7e91a47b8a2b3614
rel4t <volckovle5h@yandex.ru>
2025-07-14 14:06:45 +0300

    Добавлена регистрация хендлеров, отвечающих за вводимые параметры

--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ from bot.dispatcher import dp
 
 from handlers.admin.product.create.catalog import register_product_create_catalog
 from handlers.admin.product.create.category import register_product_create_category
+from handlers.admin.product.create.enter_params import register_product_create_enter_params
 from handlers.admin.product.create.unit import register_product_create_unit
 
 
 register_product_create_catalog(dp)
 register_product_create_category(dp)
+register_product_create_enter_params(dp)
 register_product_create_unit(dp)
 
 
