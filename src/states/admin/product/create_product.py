79b88a1f49e49fb708a86b79055834f0cafdaea5
rel4t <volckovle5h@yandex.ru>
2025-07-14 14:07:18 +0300

    Дополнена схема состояний при создании продукта

--- src/states/admin/product/create_product.py
+++ src/states/admin/product/create_product.py
@@ -16,6 +16,20 @@ class CreateProduct(StatesGroup):
     entering_category_name = State()
     confirming_category_name = State()
 
+    entering_size = State()
+
     choosing_unit = State()
     entering_new_unit = State()
     confirming_new_unit = State()
+
+    entering_count = State()
+
+    entering_price = State()
+
+    adding_desc = State()
+    entering_description = State()
+
+    adding_photo = State()
+    sending_photo = State()
+
+    confirming = State()
