3f369e941effca0f38c0a3812e4d757987cd0e06
rel4t <volckovle5h@yandex.ru>
2025-08-01 13:56:00 +0300

    Регистрация хенделера для обновления каталога

--- src/main.py
+++ src/main.py
@@ -8,12 +8,16 @@ from handlers.admin.product.create.category import admin_create_category
 from handlers.admin.product.create.enter_params import admin_create_product
 from handlers.admin.product.create.unit import admin_create_unit
 
+from handlers.admin.product.update.catalog import admin_update_catalog
+
 
 async def main():
     dp.include_router(admin_create_catalog)
     dp.include_router(admin_create_category)
     dp.include_router(admin_create_unit)
     dp.include_router(admin_create_product)
+
+    dp.include_router(admin_update_catalog)
     await dp.start_polling(bot)
 
 
