e3418e91faea47edb671e049dd329ece84fdc0ee
rel4t <volckovle5h@yandex.ru>
2025-08-01 19:13:24 +0300

    Добавлена клавиатура действий над категорией

--- src/keyboards/admin/product/update_product.py
+++ src/keyboards/admin/product/update_product.py
@@ -13,4 +13,17 @@ catalog_action_buttons = [
     ]
 ]
 
+category_action_buttons = [
+    [
+        InlineKeyboardButton(text="Изменить название", callback_data="update_category_name"), 
+    ],
+    [
+        InlineKeyboardButton(text="Не изменять название", callback_data="cancel_update_category_name"),
+    ],
+    [
+        InlineKeyboardButton(text="Назад(РАБОТАЕТ)", callback_data="back_to_choose_catalog_for_category")
+    ]
+]
+
 catalog_action_kb = InlineKeyboardMarkup(inline_keyboard=catalog_action_buttons)
+category_action_kb = InlineKeyboardMarkup(inline_keyboard=category_action_buttons)
