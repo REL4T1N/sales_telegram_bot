292682ff73334a55e6587cb858e5ed8a03683cde
rel4t <volckovle5h@yandex.ru>
2025-08-07 22:00:10 +0300

    Добавлена unit_str в функию генерации текста товара

--- src/utils/product_utils.py
+++ src/utils/product_utils.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models import Product
+from database.models import Product, Unit
+from database.database import get_db
 
 from services.product.base import get_product_display_info
 
@@ -8,7 +9,7 @@ from utils.formatting_float_nums import pretty_edit
 
 
 
-async def generate_product_info_text(db: AsyncSession, product: Product, data: dict = None) -> str:
+async def generate_product_info_text(db: AsyncSession, product: Product, data: dict = None) -> tuple[str, bool]:
     """
     Генерирует текст с информацией о продукте, учитывая возможные новые значения.
     
@@ -23,20 +24,31 @@ async def generate_product_info_text(db: AsyncSession, product: Product, data: d
     names = await get_product_display_info(db, product)
     data = data or {}  # Если data не передан, используем пустой словарь
     
+    if data.get("new_size") is not None or data.get("new_count") is not None or data.get("new_price") is not None or data.get("new_unit_id") is not None:
+        flag = True
+    else:
+        flag = False
+
     size_str = pretty_edit(product.size, data.get("new_size"))
-    quantity_str = pretty_edit(product.count, data.get("new_quantity"), " шт.")
+    count_str = pretty_edit(product.count, data.get("new_count"), " шт.")
     price_str = pretty_edit(product.price, data.get("new_price"), "₽")
+
+    async for db in get_db():
+        new_unit = await db.get(Unit, data.get("new_unit_id"))
+
+    new_unit_name = new_unit.name if new_unit else None
+    unit_str = pretty_edit(names["unit"], new_unit_name)
+
     
     text = (
         f"<b>Информация по продукту:</b>\n"
         f"<b>Каталог:</b> {names['catalog']}\n"
         f"<b>Категория:</b> {names['category']}\n"
         f"<b>Размер единицы продукции:</b> {size_str}\n"
-        f"<b>Единица измерения:</b> {names['unit']}\n"
-        f"<b>Количество продукции:</b> {quantity_str}\n"
+        f"<b>Единица измерения:</b> {unit_str}\n"
+        f"<b>Количество продукции:</b> {count_str}\n"
         f"<b>Цена:</b> {price_str}\n"
         f"<b>------</b>\n"
         f"<b>Выберите параметр для изменения:</b>"
     )
-    return text
-
+    return text, flag
