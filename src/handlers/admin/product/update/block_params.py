9adb08d369fc09064aad84308a84be40d2f46895
rel4t <volckovle5h@yandex.ru>
2025-08-07 13:33:31 +0300

    Добавлены хендлеры

--- src/handlers/admin/product/update/block_params.py
+++ src/handlers/admin/product/update/block_params.py
@@ -13,21 +13,22 @@ from database.models import Catalog, Category, Product, Unit
 
 from states.admin.product.update_product import UpdateProduct
 
-from services.product.base import get_all, get_categories_by_catalog
+from services.product.base import get_all, get_categories_by_catalog, get_product_display_data, get_product_display_info
 
 from services.product.update.base import update_object
 
 from handlers.admin.base import IsAdmin
 
 from keyboards.admin.base import create_keyboard, create_back_button, confirming_keyboard
-from keyboards.admin.product.update_product import catalog_action_kb
+from keyboards.admin.product.update_product import catalog_action_kb, generate_product_edit_keyboard
 
 from services.product.update.catalog import display_catalog_action_menu_for_update, display_catalogs_list_for_update
 from services.product.update.category import display_category_action_menu_for_update, display_category_list_for_update
-from services.product.update.block_params import generate_products_table
+from services.product.update.block_params import generate_products_table, display_products_list
 
 
 from utils.formatting_float_nums import pad, pretty_num, pretty_edit
+from utils.product_utils import generate_product_info_text
 
 admin_update_block_params = Router()
 
@@ -43,35 +44,63 @@ async def show_blocks_list(query: CallbackQuery, state: FSMContext):
     else:
         category_id = int(query.data)
         await state.update_data(category_id=category_id)
-        data = await state.get_data()
+        await display_products_list(query, state)
 
+
+@admin_update_block_params.callback_query(UpdateProduct.choosing_product)
+async def choose_product_to_update(query: CallbackQuery, state: FSMContext):
+    if query.data == "back_to_choose_category":
+        await display_category_list_for_update(
+            query,
+            state,
+            UpdateProduct.choosing_category_for_blocks,
+            text="Выберите категорию:",
+            clear_state=True
+        )
+
+    else:
+        product_id = int(query.data)
+        await state.update_data(product_id=product_id)
+        
         async for db in get_db():
-            products = await db.execute(
-                select(Product).where(Product.category_id == category_id)
-            )
-            products = products.scalars().all()
+            product = await db.get(Product, product_id)
+            text = await generate_product_info_text(db, product)
 
-            catalog = await db.get(Catalog, data["catalog_id"])
-            category = await db.get(Category, data["category_id"])
-            catalog_name = catalog.name if catalog else ""
-            category_name = category.name if category else ""
+        kb = await generate_product_edit_keyboard(show_confirm_cancel=True)
 
-            if not products:
-                await query.message.answer(f"Товары для {catalog_name} {category_name} не найдены")
-                return
-            
-            text, kb = await generate_products_table(
-                db=db,
-                products=products,
-                catalog_name=catalog_name,
-                category_name=category_name,
-                buttons_per_row=3
-            )
+        await state.set_state(UpdateProduct.choosing_product_parametr)
+        await query.message.edit_text(
+            text=text,
+            reply_markup=kb
+        )
+        await query.answer()
 
-            await state.set_state(UpdateProduct.choose_product_parametr_to_edit)
-            await query.message.edit_text(
-                text=text,
-                reply_markup=kb,
-                parse_mode="MarkdownV2"
-            )
-            await query.answer()
+
+@admin_update_block_params.callback_query(UpdateProduct.choosing_product_parametr)
+async def choose_product_param_to_update(query: CallbackQuery, state: FSMContext):
+    if query.data.startswith("edit_param:"):
+        param = query.data.split(":")[1]
+        await state.update_data(selected_param=param)
+
+        if param == "size":
+            await state.set_state(UpdateProduct.entering_new_size)
+            await query.message.edit_text(text="Введите новое значение размера:")
+
+        elif param == "unit":
+            await state.set_state(UpdateProduct.choosing_new_unit)
+            await query.message.edit_text("Выберите новую единицу измерения:")
+
+        elif param == "count":
+            await state.set_state(UpdateProduct.entering_new_count)
+            await query.message.edit_text("Введите новое количество:")
+
+        elif param == "price":
+            await state.set_state(UpdateProduct.entering_new_price)
+            await query.message.edit_text("Введите новую цену:")
+
+        await query.answer()
+
+    elif query.data == "back_to_choose_product_to_update":
+        await display_products_list(query, state)
+
+
